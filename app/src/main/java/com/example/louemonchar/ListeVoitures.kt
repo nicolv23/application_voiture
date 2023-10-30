bc34b2c21d3eae0d88f8c2bcbe9e019f7f24c58e
Nicolas Lazarte <nicolaslv0018@gmail.com>
2023-10-30 17:58:32 -0400

    Implémentation du retour en arrière entre les interfaces et insertion des données écrites dans ListeVoitures.kt

--- app/src/main/java/com/example/louemonchar/ListeVoitures.kt
+++ app/src/main/java/com/example/louemonchar/ListeVoitures.kt
@@ -10,7 +10,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-
 class ListeVoitures : Fragment() {
 
     override fun onCreateView(
@@ -30,7 +29,12 @@ class ListeVoitures : Fragment() {
             val modeles = modelesVoiture[marqueAuto]
 
             if (modeles != null && modeles.isNotEmpty()) {
-                val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, modeles)
+                val adapter = ArrayAdapter(
+                    requireContext(),
+                    R.layout.grid_item_layout,  // Utilisez le fichier de mise en page combiné ici
+                    modeles
+                )
+                adapter.setDropDownViewResource(R.layout.grid_item_layout) // Utilisez le bon fichier de mise en page ici
                 modeleGridView.adapter = adapter
             } else {
                 // Aucun modèle n'est disponible pour cette marque, affichez un message approprié
@@ -48,12 +52,19 @@ class ListeVoitures : Fragment() {
     }
 
     private fun getModelesDeVoiture(): Map<String, List<String>> {
-        // Définir votre map de modèles de voiture ici
+        // Map de modèles de voitures (codage en dur)
         val modeles: Map<String, List<String>> = mapOf(
-            "Toyota" to listOf("Corolla", "Camry", "Rav4"),
-            "Honda" to listOf("Civic", "Accord", "CR-V"),
-            "BMW" to listOf("Series 3", "Series 5", "X5"),
-            "Porsche" to listOf("Modele 1", "Modele 2", "Modele 3")
+            "Toyota" to listOf("Toyota Corolla", "Toyota Camry", "Toyota Rav4", "Toyota Prius", "Toyota Highlander", "Toyota Tundra"),
+            "Honda" to listOf("Honda Civic", "Honda Accord", "Honda CR-V", "Honda Odyssey", "Honda Pilot", "Honda Insight"),
+            "BMW" to listOf("BMW Series 3", "BMW Series 5", "BMW X5", "BMW M5", "BMW X7", "BMW Series 5"),
+            "Porsche" to listOf("Porsche 911", "Porsche 718 Cayman S", "Porsche 911 GT3", "Porsche Boxster", "Porsche Taycan", "Porsche 959"),
+            "Mazda" to listOf("Mazda CX5", "Mazda CX-30", "Mazda CX-9", "Mazda 3", "Mazda MX-5 RF", "Mazda 6"),
+            "Tesla" to listOf("Tesla Model X", "Tesla Model S", "Tesla Model 3", "Tesla Model Y", "Tesla Semi", "Tesla Cybertruck"),
+            "Lexus" to listOf("Lexus NX 350h", "Lexus IS 500", "Lexus ES 250", "Lexus LX", "Lexus ES 350", "Lexus GX 460"),
+            "Mercedes" to listOf("Mercedes-Benz Classe S", "Mercedes-Benz GLA", "Mercedes-Benz AMG GT", "Mercedes-Benz EQB", "Mercedes-Benz GLB", "Mercedes-Benz Classe C"),
+            "Jeep" to listOf("Jeep Renegade North", "Jeep Cherokee", "Jeep Avenger", "Jeep Compass", "Jeep Wrangler", "Jeep Gladiator"),
+            "Volvo" to listOf("Volvo XC60", "Volvo V60", "Volvo C40", "Volvo XC90", "Volvo S90", "Volvo EX30"),
+            "Nissan" to listOf("Nissan Pathfinder", "Nissan Rogue", "Nissan Altima", "Nissan Frontier", "Nissan Versa", "Nissan Sentra"),
         )
         return modeles
     }
@@ -63,6 +74,3 @@ class ListeVoitures : Fragment() {
         (requireActivity() as AppCompatActivity).supportActionBar?.title = title
     }
 }
-
-
-
--- app/src/main/java/com/example/louemonchar/MainActivity.kt
+++ app/src/main/java/com/example/louemonchar/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.louemonchar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,12 +16,12 @@ class MainActivity : AppCompatActivity() {
         // Récupérer le NavHostFragment (assurez-vous que l'ID correspond à celui dans votre XML)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
