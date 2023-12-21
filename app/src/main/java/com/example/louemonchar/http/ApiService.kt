cb1759c2b56774b00d859cf7558df567680e538f
ytir <ytir@dept-info.crosemont.quebec>
2023-12-20 21:03:19 -0500

    Integration de l'api du cours service reussi, mais ne fonctionne seulement en local. La version déployer sur ordralphabetix n'arrive pas à se connecter avec la base de données MYSQL Workbench.

--- app/src/main/java/com/example/louemonchar/http/ApiService.kt
+++ app/src/main/java/com/example/louemonchar/http/ApiService.kt
@@ -11,18 +11,23 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
-
+//Mock Server
 private const val BASE_URL = "https://4aa68fb2-459c-4f9c-b1d9-9c512ca251a8.mock.pstmn.io/"
+//Il faut entree son adresse IP pour se connecter à l'api, car l'api déployé sur ordralphabetix ne fonctionne pas comme il faut.
+//Il n'arrive pas à acceder à la base de données MYSQL workbench
+//private const val BASE_URL = "http://192.168.1.239:8080"
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 private val retrofit = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi)).baseUrl(BASE_URL).build()
 
 interface ApiService {
 
-    @GET("voiture")
+    @GET("/voiture")
     fun getAllAuto(): Call<List<Auto>>
+/*  On n'a pas de requête post dans le Mock server. Elle ne fonctionne pas complètement*/
+    @POST("/voitures")
+    fun enregistrerVoirure(@Body nouvVoiture: Auto): Call<Auto>
 
-  //  @POST("enregister_voiture")
-  //  fun enregistrerVoirure(@Body nouvVoiture: EnregistrerVoitureModele.nouvelleVoiture): Call<EnregistrerVoitureModele.nouvelleVoiture>
+
 }
 
 object Api{
--- app/src/main/java/com/example/louemonchar/http/Auto.kt
+++ app/src/main/java/com/example/louemonchar/http/Auto.kt
@@ -2,6 +2,8 @@ package com.example.louemonchar.http
 
 import java.sql.Date
 
+
+
 data class Auto(val code: String,
                 val code_propriétaire: String,
                 val marque: String,
@@ -12,3 +14,16 @@ data class Auto(val code: String,
                 val etat: String,
                 val prix: Int,
                 val location: String = Date((1702666669000..1712666669000).random().toLong()).toString())
+
+/*
+Version de l'api fait en service d'échange de données, mais on a pas d'utilisateur
+    data class Voiture(var code: String?,
+     val code_propriétaire: Int?,
+      val marque: String,
+      val transmission: String,
+      val modèle: String, val année: Int,
+       val image: String?,
+        val etat: String,
+       val prix: String,
+        val proprio: List<Utilisateur>? = mutableListOf())
+)*/
